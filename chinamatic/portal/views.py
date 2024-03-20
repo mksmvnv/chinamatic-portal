a2f78721b0e1e0e2f113fa4ce1fe7db5b12dc7f9
mksmvnv <mksmvnv@icloud.com>
2024-03-20 09:00:22 +0300

    Added test database of posts and contexts

--- chinamatic/portal/views.py
+++ chinamatic/portal/views.py
@@ -2,10 +2,23 @@
 
 from django.shortcuts import render
 
+db_post = [
+    {'id': 1, 'title': 'История китайского языка',
+        'description': 'Краткое описание'},
+    {'id': 2, 'title': 'Здравствуйте на китайском',
+        'description': 'Краткое описание'},
+]
+
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'posts': db_post,
+    }
+    return render(request, 'index.html', context)
 
 
 def post(request, post_id):
-    return render(request, 'post.html')
+    context = {
+        'post_id': post_id,
+    }
+    return render(request, 'post.html', context)
